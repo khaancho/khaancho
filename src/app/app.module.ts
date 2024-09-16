7a29d28f43ca36e14194f2b2ab321a54ba7b087d
milanshrestha <milan-shrestha@hotmail.com>
2024-09-16 08:50:56 +0545

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { BrandComponent } from './header/navbar/brand/brand.component';
+import { MenuComponent } from './header/navbar/menu/menu.component';
+import { SearchComponent } from './header/navbar/search/search.component';
+import { ContentComponent } from './content/content.component';
+import { SidebarComponent } from './content/sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './content/main/main.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    ContentComponent,
+    FooterComponent,
+    SidebarComponent,
+    BrandComponent,
+    MenuComponent,
+    SearchComponent,
+    MainComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +33,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    
+}
